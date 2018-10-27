8a4cdaf827ac80ee40ec4088545946c1cf2696e9
xcjthu <xiaocj16@mails.tsinghua.edu.cn>
2018-10-27 10:01:13 +0800

    add the first version cpu file

---
+++ src/pc_reg.v
@@ -0,0 +1,35 @@
+// Module:  pc_reg
+// File:    pc_reg.v
+// Description: Ö¸ÁîÖ¸Õë¼Ä´æÆ÷PC
+// Revision: 1.0
+//////////////////////////////////////////////////////////////////////
+
+`include "defines.v"
+
+module pc_reg(
+
+	input	wire										clk,
+	input wire										rst,
+	
+	output reg[`InstAddrBus]			pc,
+	output reg                    ce
+	
+);
+
+	always @ (posedge clk) begin
+		if (ce == `ChipDisable) begin
+			pc <= 32'h00000000;
+		end else begin
+	 		pc <= pc + 4'h4;
+		end
+	end
+	
+	always @ (posedge clk) begin
+		if (rst == `RstEnable) begin
+			ce <= `ChipDisable;
+		end else begin
+			ce <= `ChipEnable;
+		end
+	end
+
+endmodule
