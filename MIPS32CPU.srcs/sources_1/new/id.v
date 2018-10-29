b32686e79325a423b3c864cd6151f38ec931bbe7
xcjthu <xiaocj16@mails.tsinghua.edu.cn>
2018-10-29 20:58:52 +0800

    完成了部分算数指令的译码操作

--- MIPS32CPU.srcs/sources_1/new/id.v
+++ MIPS32CPU.srcs/sources_1/new/id.v
@@ -4,12 +4,12 @@
 
 module id(
 
-	input wire										rst,
-	input wire[`InstAddrBus]			pc_i,
-	input wire[`InstBus]          inst_i,
+	input wire rst,
+	input wire[`InstAddrBus] pc_i,
+	input wire[`InstBus] inst_i,
 
-	input wire[`RegBus]           reg1_data_i,
-	input wire[`RegBus]           reg2_data_i,
+	input wire[`RegBus] reg1_data_i,
+	input wire[`RegBus] reg2_data_i,
 
 	//送到regfile的信息
 	output reg                    reg1_read_o,
@@ -58,15 +58,96 @@ module id(
 			reg2_addr_o <= inst_i[20:16];		
 			imm <= `ZeroWord;			
 		  case (op)
-		  	`EXE_ORI:			begin                        //ORI指令
-		  		wreg_o <= `WriteEnable;		aluop_o <= `EXE_OR_OP;
-		  		alusel_o <= `EXE_RES_LOGIC; reg1_read_o <= 1'b1;	reg2_read_o <= 1'b0;	  	
-					imm <= {16'h0, inst_i[15:0]};		wd_o <= inst_i[20:16];
-					instvalid <= `InstValid;	
-		  	end 							 
-		    default:			begin
-		    end
-		  endcase		  //case op			
+		  	`EXE_ORI:
+		  	   begin                        //ORI指令
+		  		  wreg_o <= `WriteEnable;
+		  		  aluop_o <= `EXE_OR_OP;
+		  		  alusel_o <= `EXE_RES_LOGIC;
+		  		  reg1_read_o <= 1'b1;
+		  		  reg2_read_o <= 1'b0;
+				  imm <= {16'h0, inst_i[15:0]};
+				  wd_o <= inst_i[20:16];
+				  instvalid <= `InstValid;
+		  	   end //`EXE_ORI
+		    `EXE_SPECIAL_INST:
+		      begin
+		          if (op2 == 5'b0)
+		              begin
+                          wreg_o <= `WriteEnable;
+                          alusel_o <= `EXE_RES_ARITHMETIC;
+                          reg1_read_o <= 1'b1;
+                          reg2_read_o <= 1'b1;
+                          instvalid <= `InstValid;
+		                 case (op3)
+		                      `EXE_ADD:       //add指令
+		                          begin
+		                              aluop_o <= `EXE_ADD_OP;
+		                          end
+		                      `EXE_ADDU:      //addu指令
+		                          begin
+		                              aluop_o <= `EXE_ADDU_OP;
+		                          end
+		                      `EXE_SUB:       //sub
+		                          begin
+		                              aluop_o <= `EXE_SUB_OP;
+		                          end
+		                      `EXE_SUBU:      //subu
+		                          begin
+                                      aluop_o <= `EXE_SUB_OP;
+		                          end
+		                      `EXE_SLTU:      //SLTU
+		                          begin
+		                              aluop_o <= `EXE_SLTU_OP; 
+		                          end
+                              `EXE_MULT:      //mult
+                                  begin
+                                      aluop_o <= `EXE_MULT_OP;
+                                  end
+                              `EXE_MULTU:   //multi
+                                  begin
+                                      aluop_o <= `EXE_MULTU_OP;
+                                  end
+                              default:
+                                begin
+                                end
+                         endcase
+		              end // if (op2)
+		      end //`EXE
+		    `EXE_ADDI:
+		      begin
+		          
+		      end //`EXE_ADDI
+		    `EXE_ADDIU:
+		      begin
+		      end //`EXE_ADDIU
+		     `EXE_SLTI:
+		      begin
+		          
+		      end //`EXE_SLTI
+		     `EXE_SLTIU:
+		      begin
+		      end //`EXE_SLTIU
+		     `EXE_SPECIAL2_INST:
+		      begin
+		          case (op3)
+		              `EXE_CLZ:
+		                  begin
+		                  end //`EXE_CLZ
+		              `EXE_CLO:
+		                  begin
+		                  end //`EXE_CLO
+		              `EXE_MUL:
+		                  begin
+		                  end //`EXE_MUL
+		              default:
+		                  begin
+		                  end
+		          endcase //case(op3)
+		      end //`EXESPECIAL2_INST
+		    default:
+		      begin
+		      end
+		  endcase  //case op			
 		end       //if
 	end         //always
 	
